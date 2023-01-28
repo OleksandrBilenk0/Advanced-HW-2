6fea29dd553e8645cb2d07865aa8423122a8b9aa
OleksandrBilenk0 <oleksandrbilenko224@gmail.com>
2023-01-28 16:57:10 +0200

    HW with changes

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+
+let N;
+let M;
+
+do {
+  N = Number(prompt("Введіть ДОДАТНЕ, ЦІЛЕ число (N)"));
+} while (N < 0 || N !== Math.floor(N));
+do {
+  M = Number(prompt("Введіть ДОДАТНЕ, ЦІЛЕ число (M), має бути > (N)"));
+} while (
+  M < 0 || M !== Math.floor(M) || N > M
+);
+
+const evenNumber = confirm("OK - щоб пропускати парні числа");
+
 let finalValue = 0;
 
-N = Number(prompt("Введіть початкове ЦІЛЕ число"));
+for (let i = N; i <= M; i++) {
+  if (evenNumber == true && i % 2 !== 0) {
+    finalValue += i;
+  } else if (evenNumber == false) {
+    finalValue += i;
+  }
+}
+
 document.writeln("Початкове число: " + N);
 document.writeln("<br>")
-M = Number(prompt("Введіть ЦІЛЕ число до якого будемо рахувати"));
 document.writeln("Число до якого (включно) йде розрахунок: " + M);
 document.writeln("<br>")
-evenNumbers = confirm("OK - щоб пропускати парні числа");
-document.writeln("true - без парних / false - з парними: " + evenNumbers)
+document.writeln("true - без парних / false - з парними: " + evenNumber)
 document.writeln("<br>")
-
-let a = !Number.isInteger(N) || !Number.isInteger(M) ? (alert("Не уважно читаєш - числа мають бути ЦІЛИМИ!")) : (alert("Все введено вірно"));
-
-for (let i = N ; i < (M  + 1) ; i++ ){
-    if (evenNumbers)
-    { i % 2 ? finalValue += i : finalValue; }
-    else { finalValue += i; }
-};
-
 document.writeln("Результат розрахунку = " + (finalValue));
